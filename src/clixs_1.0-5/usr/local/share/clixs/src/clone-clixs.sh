e11c197c0132264aa382c4b8fa8eec354136404c
Michael Salisbury <michaelsalisbury@gmail.com>
2014-12-05 10:45:12 -0500

    build_latest.sh_clixs_1.0-5

--- src/clixs_1.0-5/usr/local/share/clixs/src/clone-clixs.sh
+++ src/clixs_1.0-5/usr/local/share/clixs/src/clone-clixs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function main(){
-	local BASE='/usr/local/share'
+	local ROOT='/usr/local/share'
 	local REPO='clixs'
 	local GIT='michaelsalisbury'
 	# verify package instalation
@@ -11,15 +11,15 @@ function main(){
 		exit 1
 	fi
 	# verify package instalation location
-	if ! [ -d "${BASE}/${REPO}" ]; then
-		echo Somethings wrong, \"${BASE}/${REPO}\" does not exist. 1>&2
+	if ! [ -d "${ROOT}/${REPO}" ]; then
+		echo Somethings wrong, \"${ROOT}/${REPO}\" does not exist. 1>&2
 		echo Maybe the \"${REPO}\" package files were removed manually. 1>&2
 		echo Exiting\! 1>&2
 		exit 2
 	fi
 	# clone or update
-	cd "${BASE}/${REPO}"
-	if [ -d "${BASE}/${REPO}/.git" ]; then
+	cd "${ROOT}/${REPO}"
+	if [ -d "${ROOT}/${REPO}/.git" ]; then
 		echo Refreshing repo. 1>&2
 		git pull
 		git fetch --all
@@ -34,25 +34,26 @@ function main(){
 		git fetch --all
 	fi
 	# update deb package from src directory
-	if ! [ -f "${BASE}/${REPO}/src/latest" ]; then
-		echo Missing \"${BASE}/${REPO}/src/latest\"\! 1>&2
+	if ! [ -f "${ROOT}/${REPO}/src/latest" ]; then
+		echo Missing \"${ROOT}/${REPO}/src/latest\"\! 1>&2
 		echo Cannot attempt to update \"${REPO}\" deb package. 1>&2
 		echo Exiting\! 1>&2
 		exit 3
 	fi
 	local CURVER=$(dpkg -p clixs | awk '/^Version:/{print $2}')
-	local LATEST=$(echo $(cat "${BASE}/${REPO}/src/latest")) 
+	echo CURVER :: ${CURVER}
+	local LATEST=$(echo $(cat "${ROOT}/${REPO}/src/latest")) 
 	if [ "${CURVER}" == "${LATEST}" ]; then
 		echo The most current version of the \"${REPO}\" package is already installed. 1>&2
 		echo DONE 1>&2
 		exit 0
-	elif ! [ -f "${BASE}/${REPO}/src/${REPO}_${LATEST}.deb" ]; then
-		echo The latest version of the \"${REPO}\" package is missing\; ${BASE}/${REPO}/src/${REPO}_${LATEST}.deb 1>&2
+	elif ! [ -f "${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb" ]; then
+		echo The latest version of the \"${REPO}\" package is missing\; ${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb 1>&2
 		echo Maybe the repo did not clone or update without error? 1>&2
 		echo Exiting\! 1>&2
 		exit 4
 	else
-		dpkg -i "${BASE}/${REPO}/src/${REPO}_${LATEST}.deb"
+		dpkg -i "${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb"
 	fi
 	
 }
