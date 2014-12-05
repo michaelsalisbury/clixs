eca153e09bc0e8f6690eccc2253a432bc2b8c773
Michael Salisbury <michaelsalisbury@gmail.com>
2014-12-05 12:01:12 -0500

    increment_revision.sh_clixs_1.0-6

---
+++ src/clixs_1.0-6/usr/local/share/clixs/src/clone-clixs.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+function main(){
+	local ROOT='/usr/local/share'
+	local REPO='clixs'
+	local GIT='michaelsalisbury'
+	# verify package instalation
+	if ! dpkg -l ${REPO} &>/dev/null; then
+		echo Looks like the \"${REPO}\" package was not installed yet. 1>&2
+		echo Exiting\! 1>&2
+		exit 1
+	fi
+	# verify package instalation location
+	if ! [ -d "${ROOT}/${REPO}" ]; then
+		echo Somethings wrong, \"${ROOT}/${REPO}\" does not exist. 1>&2
+		echo Maybe the \"${REPO}\" package files were removed manually. 1>&2
+		echo Exiting\! 1>&2
+		exit 2
+	fi
+	# clone or update
+	cd "${ROOT}/${REPO}"
+	if [ -d "${ROOT}/${REPO}/.git" ]; then
+		echo Refreshing repo. 1>&2
+		git pull
+		git fetch --all
+		git reset --hard origin/master
+	else
+		echo Cloning repo. 1>&2
+		git clone --no-checkout "git@github.com:${GIT}/${REPO}.git" tmp
+		mv tmp/.git .
+		git reset --hard origin/master
+		git stash src/clone-clixs.sh
+		git pull
+		git fetch --all
+	fi
+	# update deb package from src directory
+	if ! [ -f "${ROOT}/${REPO}/src/latest" ]; then
+		echo Missing \"${ROOT}/${REPO}/src/latest\"\! 1>&2
+		echo Cannot attempt to update \"${REPO}\" deb package. 1>&2
+		echo Exiting\! 1>&2
+		exit 3
+	else
+		local LATEST=$(echo $(cat "${ROOT}/${REPO}/src/latest"))
+	fi
+
+	if echo $(ps_reverse_tree $$ 1 --no-heading -o comm) |
+	   grep -q "^$(basename "$0") ${REPO}.postinst dpkg"; then
+		echo \"$(basename "$0")\" called from within dpkg. 1>&2
+		ps_reverse_tree $$ 4 -o pid,ppid,comm,cmd
+		local CURVER=$(ps_reverse_tree $$ 4 --no-heading -o comm,cmd |
+				awk '{if($1=="dpkg") print $NF}' |
+				xargs basename -s .deb |
+				sed "s/^${REPO}_//")
+		echo CURVER = $CURVER
+				
+		
+		#echo No need to \! 1>&2
+		return 0
+	else
+		local CURVER=$(dpkg -p clixs | awk '/^Version:/{print $2}')
+	fi
+
+	if [ "${CURVER}" == "${LATEST}" ]; then
+		echo The most current version of the \"${REPO}\" package is already installed. 1>&2
+		echo DONE 1>&2
+		exit 0
+	elif ! [ -f "${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb" ]; then
+		echo The latest version of the \"${REPO}\" package is missing\; ${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb 1>&2
+		echo Maybe the repo did not clone or update without error? 1>&2
+		echo Exiting\! 1>&2
+		exit 4
+	else
+		dpkg -i "${ROOT}/${REPO}/src/${REPO}_${LATEST}.deb"
+	fi
+	
+}
+function ps_reverse_tree(){
+	local CHILD=${1:-$$}
+	local DESCEND=${2:-2}
+	(( DESCEND == 1 )) && (( DESCEND-=1 ))
+	local PARENT
+	shift 2
+	if (( ${#@} )); then
+		local OPTS=$*
+	else
+		local OPTS="-o pid,ppid,comm,cmd"
+	fi
+	ps ${OPTS} -p ${CHILD}
+	while (( DESCEND-=1 )); do
+		PARENT=$(ps --no-heading -o ppid -p ${PARENT:-${CHILD}})
+		ps --no-heading ${OPTS/--no-heading/} -p ${PARENT}
+		(( PARENT == 1 )) && break
+	done
+
+}
+main "$@"
+exit $?
+
--- src/clone-clixs.sh
+++ src/clone-clixs.sh
@@ -1,1 +1,1 @@
-./clixs_1.0-5/usr/local/share/clixs/src/clone-clixs.sh
+./clixs_1.0-6/usr/local/share/clixs/src/clone-clixs.sh
