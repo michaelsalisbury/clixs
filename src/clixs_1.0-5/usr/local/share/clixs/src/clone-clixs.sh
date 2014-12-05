655cc272c81605c2abaf0ce7a745b70abd9fbbe8
Michael Salisbury <michaelsalisbury@gmail.com>
2014-12-05 10:59:57 -0500

    build_latest.sh_clixs_1.0-5

--- src/clixs_1.0-5/usr/local/share/clixs/src/clone-clixs.sh
+++ src/clixs_1.0-5/usr/local/share/clixs/src/clone-clixs.sh
@@ -42,8 +42,8 @@ function main(){
 	fi
         echo PID :: $PID
         echo \$\$ :: $$
-	echo ps -o pid,ppid,cmd -p $$
-	echo ps -o pid,ppid,cmd -p $(echo ps --no-heading -o ppid -p $$)
+	ps -o pid,ppid,cmd -p $$
+	ps -o pid,ppid,cmd -p $(echo ps --no-heading -o ppid -p $$)
 	local CURVER=$(dpkg -p clixs | awk '/^Version:/{print $2}')
 	echo CURVER :: ${CURVER}
 	local LATEST=$(echo $(cat "${ROOT}/${REPO}/src/latest")) 
