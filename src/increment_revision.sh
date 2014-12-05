a200c86332e8b49010eda403d6cb4381c6b8c375
Michael Salisbury <michaelsalisbury@gmail.com>
2014-12-05 10:20:36 -0500

    increment_revision.sh_clixs_1.0-3

--- src/increment_revision.sh
+++ src/increment_revision.sh
@@ -9,10 +9,11 @@ GIT="git add --all .; git commit -m $(basename "$0")_${REPO}_${UPDATE}; git push
 if [ -d "${ROOT}/${REPO}_${LATEST}" ]; then
 	echo $LATEST
 	echo $UPDATE
-	#cp -rf "${ROOT}/${REPO}_${LATEST}" "${ROOT}/${REPO}_${UPDATE}"
-	#echo ${UPDATE} > latest
-	#ln -sf "${ROOT}/${REPO}_${UPDATE}/usr/local/share/clixs/src/clone-clixs.sh" "${ROOT}/".
+	cp -rf "${ROOT}/${REPO}_${LATEST}" "${ROOT}/${REPO}_${UPDATE}"
+	echo ${UPDATE} > latest
+	ln -sf "${ROOT}/${REPO}_${UPDATE}/usr/local/share/clixs/src/clone-clixs.sh" "${ROOT}/".
 	eval "${GIT}"
+	echo
 	echo "${GIT}"
 else
 	echo Latest \"${ROOT}/${REPO}_${LATEST}\" missing. 1>&2
