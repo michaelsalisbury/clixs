585311f7c1e549f6fde2ef735ff0a12d045eacca
Michael Salisbury <michaelsalisbury@gmail.com>
2015-08-09 22:11:53 -0400

    bash

--- dev/.scripts/my-bash-scripts-update.sh
+++ dev/.scripts/my-bash-scripts-update.sh
@@ -2,13 +2,14 @@
 
 which wget &>/dev/null || { echo Please install wget. Exiting.; exit 1; }
 
-# https://raw.githubusercontent.com/michaelsalisbury/clixs/master/dev/.scripts/my-bash-scripts-updates.sh
+# https://raw.githubusercontent.com/michaelsalisbury/clixs/master/dev/.scripts/my-bash-scripts-update.sh
 GIT="https://raw.githubusercontent.com"
 USER="michaelsalisbury"
 REPO="clixs"
 BRANCH="master"
 
 function get(){
+	TRG=$1
 	SRC="${GIT}/${USER}/${REPO}/${BRANCH}/${SUB}"
 	mkdir -p "${DST}"
 	wget  -O "${DST}/${TRG}" "${SRC}/${TRG}"
@@ -16,7 +17,8 @@ function get(){
 
 # my-zfs-utils
 SUB="dev/.scripts"
-TRG="my-bash-scripts-updates.sh"
 DST="/root/.bash_scripts.d"
-get
+get "my-bash-scripts-update.sh"
+
+
 
