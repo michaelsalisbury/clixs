e3901cb6e4aa52b0172b0e44c9f7ce5a8eb48c49
Michael Salisbury <michaelsalisbury@gmail.com>
2015-08-09 22:07:23 -0400

    git

--- dev/.scripts/my-bash-scripts-updates.sh
+++ dev/.scripts/my-bash-scripts-updates.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+which wget &>/dev/null || { echo Please install wget. Exiting.; exit 1; }
 
-GIT="http://"
+# https://raw.githubusercontent.com/michaelsalisbury/clixs/master/dev/.scripts/my-bash-scripts-updates.sh
+GIT="https://raw.githubusercontent.com"
+USER="michaelsalisbury"
+REPO="clixs"
+BRANCH="master"
+
+function get(){
+	SRC="${GIT}/${USER}/${REPO}/${BRANCH}/${SUB}"
+	mkdir -p "${DST}"
+	wget  -O "${DST}/${TRG}" "${SRC}/${TRG}"
+}
+
+# my-zfs-utils
+SUB="dev/.scripts"
+TRG="my-bash-scripts-updates.sh"
+DST="/root/.bash_scripts.d"
+get
+
